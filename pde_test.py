1eed1b7d651470f01a3913944eafd0634b82a6b5
Alex Wheaton <alex.s.wheaton@gmail.com>
2020-04-15 13:52:37 +0100

    Fixed missing import.

--- pde_test.py
+++ pde_test.py
@@ -1,6 +1,7 @@
 import sys
 import time
-
+import matplotlib.pyplot as plt
+import numpy as np
 from CH_Lattice import CH_Lattice
 
 # dynamic = str(sys.argv[1])
@@ -15,6 +16,7 @@ simulation = CH_Lattice(a=0.1,M=0.1,K=0.1,dx=1.0,dt=1.0,animate=False,size=(n,m)
 # simulation.run(max_iter=max_iter)
 simulation.free_energy = []
 for step in range(max_iter):
+    print(step)
     simulation.step_forward()
 
 plt.plot(range(len(self.free_energy)), simulation.free_energy)
