fa08f1ab1425927f00fdc1c25ca999e4f4eab99a
Alex Wheaton <alex.s.wheaton@gmail.com>
2020-04-16 15:10:33 +0100

    Added Poisson lattice class and test code.

---
+++ Poisson_Lattice.py
@@ -0,0 +1,164 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from scipy import signal
+
+class Poisson_Lattice(object):
+    """
+    Class for Poisson equation solution simulation.
+    """
+
+    def __init__(self, **kwargs):
+
+        # Parameters for the simulation.
+        self.epsilon = kwargs.get("epsilon")
+        # Spatial and temporal resolution of the the simulation.
+        self.dx = kwargs.get("dx")
+        self.dt = kwargs.get("dt")
+        # Initial value for the electric potential.
+        self.phi_0 = kwargs.get("phi_0")
+        # Construct the required lattices for simulation.
+        self.size = kwargs.get("size")
+        self.build()
+
+    def build(self):
+        """
+        Generates two scalar fields and stores them as class attributes.
+        """
+        self.rho = np.zeros(self.size, dtype=float)
+        self.phi = np.zeros(self.size, dtype=float)
+
+    def make_monopole(self):
+        """
+        Create monopole at center of the charge density lattice.
+        """
+        self.rho[self.size[0]//2, self.size[1]//2, self.size[2]//2] = 1.0
+
+    def make_wire(self):
+        """
+        Create wire along the z-axis of the charge density lattice.
+        """
+        self.rho[self.size[0]//2, self.size[1]//2, :] = 1.0 / self.size[2]
+
+    def set_dirchlect_boundary(self):
+        self.phi[0,:,:], self.phi[-1,:,:] = 0.0, 0.0
+        self.phi[:,0,:], self.phi[:,-1,:] = 0.0, 0.0
+        self.phi[:,:,0], self.phi[:,:,-1] = 0.0, 0.0
+
+    def conv_laplacian_3D(self, field):
+        """
+        Calculate the laplacian at every point on the lattice simultaneously
+        by convolving with a kernel. Return the entire field of laplacians.
+        """
+        kernel = np.array([[0.0,1.0,0.0],[1.0,-4.0,1.0],[0.0,1.0,0.0]])
+        return(signal.convolve2d(field, kernel, boundary='wrap', mode='same')/self.dx**2)
+
+    def conv_gradient_3D(self, field):
+        """
+        Calculate the gradient at every point on the lattice simultaneously
+        by convolving with a kernel. Return the entire field of gradient values.
+        """
+        kernel = np.array([[0.0,-1.0,0.0],[-1.0,0.0,1.0],[0.0,1.0,0.0]])
+        return(signal.convolve2d(field, kernel, boundary='wrap', mode='same')/(2.0*self.dx))
+
+    def jacobi_step(self):
+        kernel = np.array([[[0.0,0.0,0.0],[0.0,1.0,0.0],[0.0,0.0,0.0]],
+                           [[0.0,1.0,0.0],[1.0,0.0,1.0],[0.0,1.0,0.0]],
+                           [[0.0,0.0,0.0],[0.0,1.0,0.0],[0.0,0.0,0.0]]])
+        neighbors = signal.fftconvolve(self.phi, kernel, mode='same')
+        phi_next = (neighbors + self.rho) / 6.0
+        return(phi_next)
+
+    def gauss_seidel_step(self, **kwargs):
+        kernel = np.array([[[0.0,0.0,0.0],[0.0,1.0,0.0],[0.0,0.0,0.0]],
+                           [[0.0,1.0,0.0],[1.0,0.0,1.0],[0.0,1.0,0.0]],
+                           [[0.0,0.0,0.0],[0.0,1.0,0.0],[0.0,0.0,0.0]]])
+        self.phi = (signal.fftconvolve(self.phi,kernel,mode='same') + self.rho) / 6.0
+
+        # Over relax, if over-relaxation parameter provided.
+        if "omega" in kwargs:
+            self.phi += kwargs.get("omega")
+
+    def electric_field_comp(self):
+        """
+        Calculate the x, y, and z components of the electric field in 3D space
+        and return them as three, 3D arrays of x, y, and z components.
+        """
+        E = np.gradient(self.phi)
+        return (-np.array(E)[0], -np.array(E)[1], -np.array(E)[2])
+
+    def step_forward(self, *args):
+        """
+        Steps the simulation forward one iteration using a specified
+        integration algorithm.
+        """
+        self.gauss_seidel_step()
+        self.set_dirchlect_boundary()
+
+        # Return an image object if the animation argument is enabled.
+        if self.animate == True:
+            self.steps += 1
+            print("Simulation step {}...".format(self.steps), end="\r")
+            self.image.set_array(self.phi)
+            return(self.image,)
+
+    def converged(self, ref_state, tolerance):
+        """
+        Check whether the solution has converged within some tolerance with
+        respect to some reference state.
+        """
+        if np.sum(abs(self.phi-ref_state), axis=None) <= tolerance:
+            return(True)
+        else:
+            return(False)
+
+    def run(self, **kwargs):
+        """
+        Sets up a figure, image, and FuncAnimation instance, then runs the
+        simulation to the specified maximum number of iterations.
+        """
+        # Boolean, whether or not step_forward() should return an image.
+        self.animate = kwargs.get("animate")
+        # Number of simulation steps using step_forward() method.
+        max_iter = kwargs.get("max_iter")
+        # Tolerance for convergence of the solution.
+        tolerance = kwargs.get("tol")
+
+        # Run the simulation using FuncAnimation if animate argument is enabled.
+        if self.animate == True:
+            self.figure = plt.figure()
+            self.image = plt.imshow(self.phi, cmap='viridis', animated=True)
+            plt.colorbar()
+            self.steps = 0
+            self.animation = animation.FuncAnimation(self.figure,
+                                                     self.step_forward,
+                                                     frames=max_iter,
+                                                     repeat=False,
+                                                     interval=75, blit=True
+                                                     )
+            plt.show()
+            plt.clf()
+
+        # Otherwise, manually update the simulation with step_forward() method.
+        elif self.animate == False:
+            for step in range(max_iter):
+                print("Simulation step {} of {}...".format(step, max_iter), end="\r")
+                ref_state = self.phi
+                self.step_forward()
+                if self.converged(ref_state, tolerance) == True:
+                    print("\nConvergence after {} steps!".format(step))
+                    break
+                elif step == max_iter - 1:
+                    print("\nReached max iterations w/o convergence!")
+                else:
+                    pass
+
+        return(step)
+
+    def export_animation(self, filename, dotsPerInch):
+        """
+        Exports the animation to a .gif file without compression. (Linux
+        distributions with package "imagemagick" only. Files can be large!)
+        # TODO: Add support for other image writing packages.
+        """
+        self.animation.save(filename, dpi=dotsPerInch, writer="imagemagick")
--- pde_test.py
+++ pde_test.py
@@ -4,18 +4,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from CH_Lattice import CH_Lattice
+from Poisson_Lattice import Poisson_Lattice
 
-n = int(sys.argv[1])
-m = int(sys.argv[2])
-phi_0 = float(sys.argv[3])
-max_iter=int(sys.argv[4])
+l = int(sys.argv[1])
+n = int(sys.argv[2])
+m = int(sys.argv[3])
+phi_0 = float(sys.argv[4])
+max_iter=int(sys.argv[5])
 
-simulation = CH_Lattice(a=0.1, M=0.1, K=0.1, phi_0=phi_0,
-                        dx=1.0, dt=1.0, size=(n,m)
-                        )
-simulation.run(animate=False, max_iter=max_iter)
+# simulation = CH_Lattice(a=0.1, M=0.1, K=0.1, phi_0=phi_0,
+#                         dx=1.0, dt=1.0, size=(n,m)
+#                         )
+# simulation.run(animate=False, max_iter=max_iter)
 
-#plt.plot(range(len(simulation.free_energy)), simulation.free_energy)
-#plt.title("Free Energy for phi_0={}".format(phi_0))
-#plt.savefig("plots/free_energy_phi0={}.png".format(phi_0))
+simulation = Poisson_Lattice(epsilon=1.0, phi_0=phi_0,
+                             dx=1.0, dt=1.0, size=(l,n,m)
+                             )
+simulation.make_wire()
+simulation.run(animate=False, max_iter=max_iter, tol=0.01)
+
+plt.plot(range(len(simulation.free_energy)), simulation.free_energy)
+plt.title("Free Energy for phi_0={}".format(phi_0))
+plt.savefig("plots/free_energy_phi0={}.png".format(phi_0))
 np.savetxt("data/free_energy_phi0={}.csv".format(phi_0), simulation.free_energy)
